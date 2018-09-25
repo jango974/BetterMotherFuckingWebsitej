50e23d6906ad8c28c5a2398c7bd64f85795e4195
jango974 <votre@email.com>
2018-09-25 17:13:31 +0200

    mode psychedelique

---
+++ confirm.js
@@ -0,0 +1,12 @@
+
+var choix = confirm("souhaitez vous continuer sur xxxvidsxxx?");
+if (choix === true){
+	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+else {
+	close()
+}
+
+
+
+ 
---
+++ js/Better.js
@@ -0,0 +1,37 @@
+function ouvrirPopup (page,nom,option){
+	window.open ("popup.html","pop1","width=500,height=500,left=500,top=200");
+}
+
+window.setTimeout("ouvrirPopup()",1000);
+
+
+
+
+ 
+
+
+function psychedelique (){
+ var psyke = document.querySelectorAll("p,aside");
+if( psyke.length > 0 ){
+  for(var h = 0; h < psyke.length; h++){
+    psyke[h].style['color'] = 'mediumslateblue'; 
+    psyke [h].style.fontFamily = 'papyrus';
+  }
+}else{
+   
+}
+  var titre = document.querySelectorAll("h1, h2, h3");
+  if( titre.length > 0 ){
+  for(var h = 0; h < titre.length; h++){
+    titre[h].style['color'] = 'green'
+	titre[h].style.fontFamily = 'comicsans';
+}
+}
+else{
+
+}
+
+}
+
+
+
